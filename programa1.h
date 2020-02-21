06438b0dd6d3ff35e1833b811cb9c403d6ba1fbc
josé <55531215+capitantermy@users.noreply.github.com>
2020-02-21 09:32:22 +0100

    Add files via upload

---
+++ programa1.h
@@ -0,0 +1,23 @@
+ 
+ 
+ 
+ char palabra[10];
+    char salir;
+    int cont;
+    int longitud;
+    longitud=0;//iniciando variable
+    std::cout<<"Dime tu nombre:";
+    std::cin>> palabra;
+    //dirty trick
+    while(palabra[cont]!='\0'){
+                               longitud++;
+              
+    }
+    
+    std::cout<<"Elnombre de ";
+    std::cout<< palabra;
+    std::cout<<"tiene";
+    std::cout<<longitud;
+    std::cout<<"letras";
+    
+    std::cin>>salir;
